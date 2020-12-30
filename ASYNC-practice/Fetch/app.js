68b61d61c8a29a05cd790e409d83fd6b73c1188e
kenderb <40863532+kenderb@users.noreply.github.com>
2020-12-30 17:48:40 -0500

    Add some semicolon to the lines

--- ASYNC-practice/Fetch/app.js
+++ ASYNC-practice/Fetch/app.js
@@ -16,8 +16,8 @@ const fetchBitcoinPrice = async () => {
     try {
         const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
         const data = await res.json();
-        console.log(data.ticker.price)
+        console.log(data.ticker.price);
     } catch (e) {
-        console.log("SOMETHING WENT WRONG!!!", e)
+        console.log("SOMETHING WENT WRONG!!!", e);
     }
 }
